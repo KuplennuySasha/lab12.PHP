3d6f6d0b268d2c4183a748e74dd0e6168b79a656
Sasha <KuplennuySasha@i.ua>
2020-05-08 14:12:02 +0300

    first commit

---
+++ add.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "phone";
+
+$name=$_GET['name'];
+$phone=$_GET['phone'];
+$street=$_GET['street'];
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO tbl_phone (name, phone, street)
+    VALUES ('$name', '$phone', '$street')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
